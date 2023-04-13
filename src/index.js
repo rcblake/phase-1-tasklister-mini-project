5eebaa3dd17a963007398a9562b3774d2d1362e6
Ren Blake <rcblake06@gmail.com>
2023-04-13 04:15:08 -0700

    edit creates new task and removes the previous

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form").addEventListener("submit",(e) => {e.preventDefault()
+    createTask(e.target.newTaskDescription.value,e.target.newTaskPriority.value)
+  })
 });
+
+function createTask(task,taskPriority) {
+  const p = document.createElement("p")
+  const checkBox = document.createElement("input")
+  const editButton = document.createElement("button")
+  checkBox.type = "checkbox"
+  p.innerText = task
+  p.name = "taskDescription"
+  p.className = taskPriority
+  editButton.className = "taskEditButton"
+  editButton.innerText = "Edit"
+  editButton.addEventListener("click",(e) => {editTask(p,taskPriority)})
+
+  p.prepend(checkBox)
+  p.append(editButton)
+  document.getElementById("tasks").appendChild(p)
+
+  console.log(`Task: ${task} Priority: ${taskPriority}`)
+}
+
+function editTask(oldTask,oldTaskPriority) {
+  let newTask = prompt("Enter Edit");
+  if (newTask !== null) { 
+    oldTask.remove();
+    createTask(newTask, oldTaskPriority)
+  }
+}
